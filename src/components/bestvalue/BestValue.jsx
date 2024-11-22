18abb6462723edd9cfea155422b353e9100db6a6
Liyana kp <liyana.kp8590@gmail.com>
2024-11-23 00:25:50 +0530

    feat: progres

--- src/components/bestvalue/BestValue.jsx
+++ src/components/bestvalue/BestValue.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import './BestValue.css'
+import { DataContext } from "../../layout/DataProvider";
 
 export const BestValue = () =>{
+    
+    const data = useContext(DataContext);
+    
+
+    const jwelery= data.filter(item=> item.category === "jewelery")
+    
 
 
     return(
@@ -12,6 +19,11 @@ export const BestValue = () =>{
                 <p>Your One-stop Online Shopping Site For Clothes,
                      Accessories Footwear & More</p>
             </div>
+        <div className="jwellery">
+            {jwelery.length > 0 ? (jwelery.slice(0,4).map((item)=>(<div key={item.id} className="jwellery-box" >
+                <img src={item.image} alt={item.name} className="jwellery-img"></img>
+            </div>))) : (<p>No jwelery items</p>)}
+        </div>
         </div>
     )
 }
